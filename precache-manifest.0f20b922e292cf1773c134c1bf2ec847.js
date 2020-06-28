176b7116435f509c7f0cca28174928b1aba88f17
BENAHMED DAHO Ali <bidandou@yahoo.fr>
2020-06-29 00:35:41 +0100

    Updates

---
+++ precache-manifest.0f20b922e292cf1773c134c1bf2ec847.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430805ec18c5b02c1fb76b0a5ffeeee6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41f07c7cab50a2ec65c8",
+    "url": "/static/css/main.318d15dc.chunk.css"
+  },
+  {
+    "revision": "6fa3e51975cba4543322",
+    "url": "/static/js/2.52e42229.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.52e42229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f07c7cab50a2ec65c8",
+    "url": "/static/js/main.860b0525.chunk.js"
+  },
+  {
+    "revision": "6a7827e0db66b0f0efd0",
+    "url": "/static/js/runtime-main.7e387834.js"
+  },
+  {
+    "revision": "01067895a750dd4d27aafbfbbd9425f6",
+    "url": "/static/media/logo-coccinelle.01067895.png"
+  }
+]);
