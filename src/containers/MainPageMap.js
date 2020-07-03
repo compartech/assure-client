88505d4a4d05e31e55a1b0044a56307f489ce323
BENAHMED DAHO Ali <bidandou@yahoo.fr>
2020-07-04 00:18:05 +0100

    Add auto center the map using onLocate

--- src/containers/MainPageMap.js
+++ src/containers/MainPageMap.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useState, createRef, useEffect } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
@@ -8,27 +8,48 @@ import {
   Marker,
   Popup,
   TileLayer,
-  Tooltip,
-} from 'react-leaflet';
+  } from 'react-leaflet';
 
-const center = [33.0, 3.0]
+const initCenter = [33.0, 3.0]
 
 const MainPageMap = () => {
+
+  const [center, setCenter] = useState(initCenter);
+  const [hasLocation, setHasLocation] = useState(false);
+  const mapRef = createRef();
+
+  useEffect(() => {
+    const map = mapRef.current
+    if (map != null) {
+      map.leafletElement.locate();
+    }});
+  
+  const handleLocationFound = (e) => {
+    setHasLocation(true);
+    setCenter(e.latlng);
+  }
+
+  const marker = hasLocation ? (
+    <Marker position={center}>
+      <Popup>You are here</Popup>
+    </Marker>
+  ) : null
 
   return (
     <Grid item xs={12}
       style={{ textAlign: "center", padding: "1rem" }}
     >
       <Paper>
-        <Map center={center} zoom={5}>
+        <Map center={center} 
+             zoom={8}
+             onLocationfound={handleLocationFound}
+             ref={mapRef}
+             >
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[36.60, 3.0]}>
-            <Popup>Company</Popup>
-            <Tooltip>Agence</Tooltip>
-          </Marker>
+          {marker}
         </Map>
       </Paper>
     </Grid>
